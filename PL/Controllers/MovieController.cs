9d3a191ecb683fd8ed2a9b4ee447effde0fd4d96
MSandoval00 <msandovalg00@gmail.com>
2023-10-02 18:57:31 -0600

    Add project files.

---
+++ PL/Controllers/MovieController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PL.Controllers
+{
+    public class MovieController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            //Models.Result result = new Models.Result();
+            Models.Root root=new Models.Root();
+            root.results = new List<Models.Movie>();
+            using (var client =new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://api.themoviedb.org/3/movie/popular?");
+                var responseTask = client.GetAsync("api_key=31e541d91f4612df7a43bcaf3cfe4be3");
+                responseTask.Wait();
+
+                var resultService=responseTask.Result;
+                if (resultService.IsSuccessStatusCode)
+                {
+                    var readTask = resultService.Content.ReadAsAsync<Models.Root>();
+                    readTask.Wait();
+                    foreach (var resultMovie in readTask.Result.results)
+                    {
+                        Models.Movie resultItemList = Newtonsoft.Json.JsonConvert.DeserializeObject<Models.Movie>(resultMovie.ToString());
+                        root.results.Add(resultItemList);
+                    }
+                }
+
+
+            }
+
+            return View(root);
+        }
+    }
+}
