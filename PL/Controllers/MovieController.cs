e0e7ecb6ef9952fe9ea57ae09403a3298391d62f
MSandoval00 <msandovalg00@gmail.com>
2023-10-03 20:30:01 -0600

    WebApi Movies

--- PL/Controllers/MovieController.cs
+++ PL/Controllers/MovieController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.Net.Http;
 
 namespace PL.Controllers
 {
@@ -7,16 +10,16 @@ namespace PL.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            //Models.Result result = new Models.Result();
-            Models.Root root=new Models.Root();
+            
+            Models.Root root = new Models.Root();
             root.results = new List<Models.Movie>();
-            using (var client =new HttpClient())
+            using (var client = new HttpClient())
             {
                 client.BaseAddress = new Uri("https://api.themoviedb.org/3/movie/popular");
                 var responseTask = client.GetAsync("?api_key=31e541d91f4612df7a43bcaf3cfe4be3");
                 responseTask.Wait();
 
-                var resultService=responseTask.Result;
+                var resultService = responseTask.Result;
                 if (resultService.IsSuccessStatusCode)
                 {
                     var readTask = resultService.Content.ReadAsAsync<Models.Root>();
@@ -24,7 +27,7 @@ namespace PL.Controllers
                     foreach (var resultMovie in readTask.Result.results)
                     {
                         root.results.Add(resultMovie);
-                        //root.results.Add(resultItemList);
+
                     }
                 }
 
@@ -33,5 +36,135 @@ namespace PL.Controllers
 
             return View(root);
         }
+        [HttpGet]
+        public ActionResult GetAllSin(int? page)
+        {
+
+            Models.MovieS movieS = new Models.MovieS();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://api.themoviedb.org/3/movie/popular");
+                //var responseTask = client.GetAsync("");
+                string url = "";
+                //if (page == null)
+                //{
+                    page = page==null ? 1:page;
+                    url = "?api_key=31e541d91f4612df7a43bcaf3cfe4be3&page=" + page;
+                //}
+                var responseTask = client.GetAsync(url);
+                responseTask.Wait();
+
+                var resultService = responseTask.Result;
+                if (resultService.IsSuccessStatusCode)
+                {
+                    var readTask = resultService.Content.ReadAsStringAsync();
+                    dynamic resultJSON = JObject.Parse(readTask.Result.ToString());
+                    readTask.Wait();
+                    movieS.Movies = new List<object>();
+                    foreach (var resultItem in resultJSON.results)
+                    {
+                        Models.MovieS movieItem = new Models.MovieS();
+                   
+                        movieItem.id = resultItem.id;
+                        movieItem.title = resultItem.title;
+                        movieItem.release_date = resultItem.release_date;
+                        movieItem.original_title = resultItem.original_title;
+                        movieItem.overview = resultItem.overview;
+                        movieItem.popularity = resultItem.popularity;
+                        movieItem.backdrop_path = "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/" + resultItem.backdrop_path;
+                        movieS.Movies.Add(movieItem);
+
+                    }
+                }
+
+
+            }
+            ViewBag.page = page;
+            return View(movieS);
+        }
+        
+        
+        public ActionResult AddFavorite(int IdMovie, bool Favorite)
+        {
+            
+            using (var client = new HttpClient())
+            {
+  
+                client.BaseAddress = new Uri("https://api.themoviedb.org/3/account/");
+                var json = new
+                {
+                    media_type = "movie",
+                    media_id = IdMovie,
+                    favorite = Favorite
+                };
+                var postTask = client.PostAsJsonAsync("20522403/favorite?session_id=4daa41461d611b36e69708ce658629a4f87c3595&api_key=31e541d91f4612df7a43bcaf3cfe4be3",json);
+                postTask.Wait();
+                var resultMovie=postTask.Result;
+                if (Favorite)
+                {
+                    if (resultMovie.IsSuccessStatusCode)
+                    {
+                        ViewBag.Mensaje = "Agregado a favoritos";
+                    }
+                }
+                else
+                {
+                    if (resultMovie.IsSuccessStatusCode)
+                    {
+                        ViewBag.Mensaje = "Eliminado de favoritos";
+                    }
+                }
+                
+                return View("Modal");
+            }
+        }
+
+        public ActionResult GetAllFavorite(int? page)
+        {
+            Models.MovieS movieS = new Models.MovieS();
+            using (var client = new HttpClient())
+            {
+
+                client.BaseAddress = new Uri("https://api.themoviedb.org/3/");
+
+                string url = "";
+                if (page == null)
+                {
+                    url = "account/20522454/favorite/movies?api_key=31e541d91f4612df7a43bcaf3cfe4be3&session_id=4daa41461d611b36e69708ce658629a4f87c3595";
+                }
+
+
+
+                var responseTask = client.GetAsync(url);
+                responseTask.Wait();
+
+                var resultService = responseTask.Result;
+                if (resultService.IsSuccessStatusCode)
+                {
+                    var readTask = resultService.Content.ReadAsStringAsync();
+                    dynamic resultJSON = JObject.Parse(readTask.Result.ToString());
+                    readTask.Wait();
+                    movieS.Movies = new List<object>();
+                    foreach (var resultItem in resultJSON.results)
+                    {
+                        Models.MovieS movieItem = new Models.MovieS();
+                        movieItem.id = resultItem.id;
+                        movieItem.title = resultItem.title;
+                        movieItem.release_date = resultItem.release_date;
+                        movieItem.original_title = resultItem.original_title;
+                        movieItem.overview = resultItem.overview;
+                        movieItem.popularity = resultItem.popularity;
+                        movieItem.backdrop_path = "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/" + resultItem.backdrop_path;
+                        movieS.Movies.Add(movieItem);
+
+                    }
+                }
+            }
+            ViewBag.page = page;
+            return View(movieS);
+        }
+
     }
 }
+
--- PL/Models/MovieS.cs
+++ PL/Models/MovieS.cs
@@ -1,19 +1,16 @@
 ﻿namespace PL.Models
 {
-    public class Movie
+    public class MovieS
     {
+        public int page { get; set; }
         public int id { get; set; }
-        public bool adult { get; set; }
         public string backdrop_path { get; set; }
-        public List<int> genre_ids { get; set; }
         public string original_title { get; set; }
         public string overview { get; set; }
         public decimal popularity { get; set; }
-        public string poster_path { get; set; }
         public string release_date { get; set; }
         public string title { get; set; }
-        public bool video { get; set; }
-        public float vote_average { get; set; }
-        public int vote_count { get; set; }
+     
+        public List<object> Movies { get; set; }
     }
 }
